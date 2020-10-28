1675288787cfe1f689390e3126369fd3095c560b
Selma Elgabalawy <salmosha88@gmail.com>
2020-10-28 15:19:31 -0700

    updates

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // Adds the current day of the week and date to the jumbotron
+    $("#currentDay").text(moment().format('dddd, MMMM Do'));
+
+    var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+});
